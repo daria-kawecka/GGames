7686787f40aa2dd1df019e0d4e679a8fd5e07ea6
daria-kawecka <dusiek2806@gmail.com>
2020-12-16 01:43:27 +0100

    add gsap to location section

--- src/components/location.js
+++ src/components/location.js
@@ -1,17 +1,39 @@
-import { createFactory } from "react";
 import Map from "../components/Map";
-
 import { ReactComponent as WaveBlack } from "../images/wave_black.svg";
+
+import { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import "../styles/location.scss";
 
 const Location = () => {
+  const scene = useRef(null);
+
+  useEffect(() => {
+    const element = scene.current;
+    let elements = element.children;
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.from(elements, {
+      x: 1500,
+      duration: 1,
+      ease: "power2.out",
+      stagger: 0.6,
+      scrollTrigger: {
+        trigger: elements,
+        start: "top bottom",
+        toggleActions: "play pause resume reset",
+      },
+    });
+  });
+
   return (
     <>
       <p className="locationSection__header">
         Find us <span className="locationSection__header--span">easily</span>
       </p>
-      <div class="locationSection__container">
+      <div class="locationSection__container" ref={scene}>
         <div className="locationSection__mapContainer">
           <Map></Map>
         </div>
