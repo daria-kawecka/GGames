f710bd7c85f1f3454985a59143f6d27bf488f13c
daria-kawecka <dusiek2806@gmail.com>
2020-12-24 01:38:15 +0100

    add games component

--- src/components/Games.js
+++ src/components/Games.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const gamesApi = "https://api.igdb.com/v4/games";
 const ClientID = "4pxockamizj9lw8cvb53aji4y3dppk";
-const 
+// const
 const Games = () => {
   return <p>Gieeery</p>;
 };
