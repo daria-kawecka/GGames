af0a47b59c7b400cbfa1766d69567c93ace498e0
daria-kawecka <dusiek2806@gmail.com>
2021-01-01 22:08:07 +0100

    new api

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import db from "../db.json";
 import axios from "axios";
 
 const gamesApi = "https://api.igdb.com/v4/games";
@@ -9,46 +10,37 @@ const Games = () => {
   const [games, setGames] = useState([]);
 
   useEffect(() => {
-    axios({
-      url: "https://api.igdb.com/v4/age_ratings",
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Access-Control-Allow-Origin": true,
-        Accept: "application/x-www-form-urlencoded",
-        "Client-ID": "4pxockamizj9lw8cvb53aji4y3dppk",
-        Authorization: "Bearer 02y1rsig8h6r7iwo5xaxkyas3of54v",
-      },
-      routes: {
-        cors: true,
-      },
+    // axios
+    //   .get(gamesApi, {
+    //     credentials: "include",
+    //     headers: {
+    //       "Access-Control-Allow-Origin": true,
+    //       Accept: "application/x-www-form-urlencoded",
+    //       "Client-ID": "4pxockamizj9lw8cvb53aji4y3dppk",
+    //       Authorization: "Bearer 02y1rsig8h6r7iwo5xaxkyas3of54v",
+    //     },
+    //     routes: {
+    //       cors: true,
+    //     },
 
-      data:
-        "fields category,checksum,content_descriptions,rating,rating_cover_url,synopsis;",
-    })
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((err) => {
-        // console.error(err);
-      });
+    //     data:
+    //       "fields category,checksum,content_descriptions,rating,rating_cover_url,synopsis;",
+    //   })
+    //   .then((response) => {
+    //     console.log(response.data);
+    //   })
+    //   .catch((err) => {
+    //     // console.error(err);
+    //   });
+
+    axios
+      .get(
+        "https://www.giantbomb.com/api/games/?api_key=b7d9a5a1b5d4d97811a92bebf8e480a3c0e20143&limit=10&format=json"
+      )
+      .then((response) => console.log(response.data.results))
+      .then((data) => console.log(data));
   }, []);
-
-  //   useEffect(() => {
-  //     fetch(gamesApi, {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         "Client-ID": ClientID,
-  //         Authorization: Auth,
-  //       },
-  //     })
-  //       .then((res) => res.json)
-  //       .then((data) => {
-  //         console.log(data);
-  //         setGames(data);
-  //       });
-  //   }, []);
+  // console.log(db);
   return <p>Gieeery</p>;
 };
 
