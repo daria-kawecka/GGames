b040d923257054f78eb6f8d0072ff53a29dc5249
daria-kawecka <dusiek2806@gmail.com>
2021-01-03 22:03:31 +0100

    change styles

--- src/components/Games.js
+++ src/components/Games.js
@@ -3,14 +3,21 @@ import React, { useEffect, useState } from "react";
 import "../styles/Games.scss";
 
 const Games = ({ name, image }) => {
-  console.log(image.small_url);
-  const img = image.small_url;
+  const img = image.medium_url;
   const desc = image.image_tags;
+  const price = Math.floor((Math.random() * 20 + 2) * 5);
+  console.log("cena : " + price);
   return (
     <>
       <div className="games__gameContainer">
         <p className="games__name">{name}</p>
         <img className="games__img" src={img} alt={desc} />
+        <div className="games__price">
+          <p class="games__price__text">For now game costs... </p>
+          <p className="games__price__money">
+            {price} <span>$$$</span>
+          </p>
+        </div>
       </div>
     </>
   );
--- src/pages/Games_Store.js
+++ src/pages/Games_Store.js
@@ -2,10 +2,12 @@ import Games from "../components/Games.js";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import { ReactComponent as Wave } from "../images/wave.svg";
+
 const Games_Store = () => {
   const [games, setGames] = useState([]);
   const gamesApi =
-    "https://www.giantbomb.com/api/games/?api_key=b7d9a5a1b5d4d97811a92bebf8e480a3c0e20143&limit=10&format=json";
+    "https://www.giantbomb.com/api/games/?api_key=b7d9a5a1b5d4d97811a92bebf8e480a3c0e20143&limit=30&format=json&sort=date_last_updated:desc";
 
   // dobry = https://www.giantbomb.com/api/games/?api_key=b7d9a5a1b5d4d97811a92bebf8e480a3c0e20143&limit=10&format=json
 
