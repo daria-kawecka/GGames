cde597469a38927ee0923bdb5051e363b478fcaa
Daria Kawecka <dusiek2806@gmail.com>
2021-01-09 03:10:31 +0100

    add email sending

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,19 +1,69 @@
 import React, { useState, useEffect } from "react";
 // 9b1015ef-1aac-48a2-855d-61e89b65e303 <--- token
 const Contact = () => {
-  const sendEmail = () => {
+  const [data, setData] = useState([]);
+
+  const formRefill = (e) => {
+    const { name, value } = e.target;
+    setData((prevData) => ({ ...prevData, [name]: value }));
+  };
+  console.log(data.subject);
+  const sendEmail = (e) => {
+    e.preventDefault();
+
     window.Email.send({
       SecureToken: "9b1015ef-1aac-48a2-855d-61e89b65e303",
-      To: "tutajtestuje@gmail.com",
+      To: data.email,
       From: "tutajtestuje@gmail.com",
-      Subject: "This is the subject",
-      Body: "And this is the body",
-    }).then((message) => alert("wysłano email"));
+      Subject: data.subject,
+      Body: `<h1>Cześć</h1> ${data.name}!`,
+    }).then(alert("wysłano email"));
   };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <h1>Contact page</h1>
-      <button onClick={sendEmail}>Send</button>
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Your name..."
+          onChange={formRefill}
+          required
+        />
+        <input
+          type="email"
+          placeholder="Your email..."
+          name="email"
+          onChange={formRefill}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Subject"
+          name="subject"
+          onChange={formRefill}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Your Phone Number (optional)"
+          name="number"
+        />
+        <input type="text" placeholder="Your Web Site (optional)" name="web" />
+        <textarea
+          name="content"
+          cols="30"
+          rows="10"
+          placeholder="Your message..."
+          required
+        ></textarea>
+
+        <button onClick={sendEmail}>Send</button>
+      </form>
     </>
   );
 };
