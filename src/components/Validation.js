b2ba072d0207fdc24b29b90713a8d7fb3a53b0bb
Daria Kawecka <dusiek2806@gmail.com>
2021-01-13 02:11:54 +0100

    validation function

--- src/components/Validation.js
+++ src/components/Validation.js
@@ -1,28 +1,40 @@
 export default function validate(values) {
+	console.log("walidacja");
 	let email, name, subject, content;
-	let filled = true;
-
-	if (!values.email) {
-		email = "Email is required!";
-		filled = false;
-	} else if (!/\S+@\S+\.\S+/.test(values.email)) {
-		email = "Email address is invalid...";
-		filled = false;
-	}
-	if (!values.name) {
-		name = "Name is required!";
-		filled = false;
-	}
-	if (!values.subject) {
-		subject = "Subject is required!";
-		filled = false;
-	}
-	if (!values.content) {
-		content = "Please write a message...";
-		filled = false;
-	} else if (values.content.split(" ").length < 5) {
-		content = "Message should be longer than 5 words...";
-		filled = false;
+	let filled = false;
+	if (values.email && values.name && values.subject && values.content) {
+		filled = true;
+		if (!/\S+@\S+\.\S+/.test(values.email)) {
+			email = "Email address is invalid...";
+			filled = false;
+		}
+		if (values.content.split(" ").length < 5) {
+			content = "Message should be longer than 5 words...";
+			filled = false;
+		}
+	} else {
+		if (!values.email) {
+			email = "Email is required!";
+			filled = false;
+		} else if (!/\S+@\S+\.\S+/.test(values.email)) {
+			email = "Email address is invalid...";
+			filled = false;
+		}
+		if (!values.name) {
+			name = "Name is required!";
+			filled = false;
+		}
+		if (!values.subject) {
+			subject = "Subject is required!";
+			filled = false;
+		}
+		if (!values.content) {
+			content = "Please write a message...";
+			filled = false;
+		} else if (values.content.split(" ").length < 5) {
+			content = "Message should be longer than 5 words...";
+			filled = false;
+		}
 	}
 	return { filled, email, name, subject, content };
 }
