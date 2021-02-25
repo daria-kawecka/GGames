82c55fc0f30d444251795088d535019dd53a6b37
daria-kawecka <dusiek2806@gmail.com>
2021-02-25 22:04:14 +0100

    add button to header

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,16 +1,22 @@
 import Header from "../components/Header";
 
 const Main = () => {
-	const text = (
-		<h1>
-			<span>GG</span>ames
-		</h1>
-	);
-	return (
-		<div className="mainPage">
-			<Header text={text} />
-		</div>
-	);
+  const text = (
+    <h1>
+      <span>GG</span>ames
+    </h1>
+  );
+  const button = (
+    <button className="button">
+      {" "}
+      <a href="/#/about-us">About us </a>
+    </button>
+  );
+  return (
+    <div className="mainPage">
+      <Header text={text} button={button} />
+    </div>
+  );
 };
 
 export default Main;
